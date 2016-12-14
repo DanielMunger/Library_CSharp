d4802756b9eb781b53584e285f55214a68b4fb8f
Daniel Munger and Levi Bibo <daniel+levi@>
2016-12-14 10:33:43 -0800

    add methods/tests for Find, Update, Save to Author class.

---
+++ Objects/Author.cs
@@ -0,0 +1,200 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace Library.Objects
+{
+  public class Author
+  {
+
+    private int _id;
+    private string _firstName;
+    private string _lastName;
+
+    public Author(string FirstName, string LastName, int Id = 0)
+    {
+      _id = Id;
+      _firstName = FirstName;
+      _lastName = LastName;
+    }
+
+    public string GetFirstName()
+    {
+      return _firstName;
+    }
+    public string GetLastName()
+    {
+      return _lastName;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public override bool Equals(Object otherAuthor)
+    {
+      if (!(otherAuthor is Author))
+      {
+        return false;
+      }
+      else
+      {
+        Author newAuthor = (Author) otherAuthor;
+        bool authorIdEquality = (_id == newAuthor.GetId());
+        bool authorFirstNameEquality = (_firstName == newAuthor.GetFirstName());
+        bool authorLastNameEquality = (_lastName == newAuthor.GetLastName());
+        return (authorIdEquality && authorFirstNameEquality && authorLastNameEquality);
+      }
+    }
+    public override int GetHashCode()
+    {
+      return _firstName.GetHashCode();
+    }
+
+    public void Save()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("INSERT INTO authors (first_name, last_name) OUTPUT INSERTED.id VALUES (@FirstName, @LastName);", conn);
+
+      cmd.Parameters.AddWithValue("@FirstName", this.GetFirstName());
+      cmd.Parameters.AddWithValue("@LastName", this.GetLastName());
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+    }
+
+    public static List<Author> GetAll()
+    {
+      List<Author> allAuthors = new List<Author>{};
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("SELECT * FROM authors;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int AuthorId = rdr.GetInt32(0);
+        string AuthorFirstName = rdr.GetString(1);
+        string AuthorLastName = rdr.GetString(2);
+        Author newAuthor = new Author(AuthorFirstName, AuthorLastName, AuthorId);
+        allAuthors.Add(newAuthor);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allAuthors;
+    }
+
+    public static Author Find(int id)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT first_name, last_name FROM authors WHERE id = @Id;", conn);
+      cmd.Parameters.AddWithValue("@Id", id);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      string firstName = null;
+      string lastName = null;
+      while(rdr.Read())
+      {
+        firstName = rdr.GetString(0);
+        lastName = rdr.GetString(1);
+      }
+      Author foundAuthor = new Author(firstName, lastName, id);
+
+      if (rdr != null) rdr.Close();
+      if (conn != null) conn.Close();
+
+      return foundAuthor;
+    }
+    public void Update(string firstName, string lastName)
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("UPDATE authors SET first_name = @FirstName, last_name = @LastName WHERE id = @AuthorId;", conn);
+      cmd.Parameters.AddWithValue("@FirstName", firstName);
+      cmd.Parameters.AddWithValue("@LastName", lastName);
+      cmd.Parameters.AddWithValue("@AuthorId", this.GetId());
+
+      cmd.ExecuteNonQuery();
+      this._firstName = firstName;
+      this._lastName = lastName;
+      if (conn != null) conn.Close();
+    }
+//
+//     public static List<TEMPLATE> Sort()
+//     {
+//       List<Task> allTEMPLATE = new List<Task>{};
+//
+//       SqlConnection conn = DB.Connection();
+//       conn.Open();
+//
+//       SqlCommand cmd = new SqlCommand("SELECT * FROM template ORDER BY TEMPLATEdate;", conn);
+//       SqlDataReader rdr = cmd.ExecuteReader();
+//
+//       while(rdr.Read())
+//       {
+//         int TEMPLATEId = rdr.GetInt32(0);
+//         string TEMPLATEDescription = rdr.GetString(1);
+//         TEMPLATE newTEMPLATE = new TEMPLATE(TEMPLATEDescription, TEMPLATEId);
+//         allTEMPLATE.Add(newTEMPLATE);
+//       }
+//
+//       if (rdr != null)
+//       {
+//         rdr.Close();
+//       }
+//       if (conn != null)
+//       {
+//         conn.Close();
+//       }
+//
+//       return allTEMPLATE;
+//     }
+//     public void Delete()
+//     {
+//       SqlConnection conn = DB.Connection();
+//       conn.Open();
+//       SqlCommand cmd = new SqlCommand("DELETE FROM template WHERE id = @TEMPLATEId; DELETE FROM join_table WHERE template_id = @TEMPLATEId", conn);
+//       SqlParameter TEMPLATEIdParameter = new SqlParameter("@TEMPLATEId", this.Id);
+//       cmd.Parameters.Add(TEMPLATEIdParameter);
+//       cmd.ExecuteNonQuery();
+//
+//       if(conn!=null)
+//       {
+//         conn.Close();
+//       }
+//     }
+    public static void DeleteAll()
+    {
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+      SqlCommand cmd = new SqlCommand("DELETE FROM authors;", conn);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+    }
+
+  }
+}
---
+++ Tests/AuthorTest.cs
@@ -0,0 +1,51 @@
+using Xunit;
+using System;
+using System.Collections.Generic;
+using Library.Objects;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Library.Test
+{
+  public class AuthorTest :IDisposable
+  {
+    public AuthorTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=library_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Add_AddsAuthorToDB_True()
+    {
+      Author newAuthor = new Author("Stephen", "King");
+      newAuthor.Save();
+      List<Author> result = Author.GetAll();
+
+      Assert.Equal(1, result.Count);
+    }
+
+    [Fact]
+    public void Find_GetAuthorFromDB_EquivalentAuthor()
+    {
+      Author newAuthor = new Author("Stephen", "King");
+      newAuthor.Save();
+      Author result = Author.Find(newAuthor.GetId());
+
+      Assert.Equal(newAuthor, result);
+    }
+    [Fact]
+    public void Update_UpdatesAuthorinDB_True()
+    {
+      Author newAuthor = new Author("Steven", "King");
+      newAuthor.Save();
+      newAuthor.Update("Stephen", "King");
+      Author foundAuthor = Author.Find(newAuthor.GetId());
+      Assert.Equal(newAuthor,foundAuthor);
+    }
+    public void Dispose()
+    {
+      Author.DeleteAll();
+    }
+
+  }
+}
